dc3ea11004060cfa6b471e73f8443bb4ee03bf7c
R. S. Doiel <rsdoiel@gmail.com>
2017-12-01 11:07:32 -0800

    debugging string command

---
+++ test_cmds.bash
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+
+function assert_same() {
+    if [ "$#" != "3" ]; then
+        echo "asset_same expects 3 parmaters, LABEL EXPECTED RESULT"
+        exit 1
+    fi
+    if [ "${2}" != "${3}" ]; then
+        echo "${1}: expected |${2}|, got |${3}|"
+        exit 1
+    fi
+}
+
+#
+# These are some tests to check the datatools command
+# and show how they might be used.
+#
+function test_string() {
+    # Testing split
+    NAME="Doiel, Robert"
+    EXPECTED="[\"Doiel\",\"Robert\"]"
+    RESULT=$(bin/string split ", " "$NAME")
+    assert_same "split on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "$NAME" | bin/string -i - split ", ")
+    assert_same "split on pipe" "$EXPECTED" "$RESULT"
+
+    EXPECTED="[\"Doiel, Robert\"]"
+    RESULT=$(bin/string splitn ", " 1 "$NAME")
+    assert_same "splitn on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "$NAME" | bin/string -i - splitn ", " 1)
+    assert_same "splitn on pipe" "$EXPECTED" "$RESULT"
+
+    # Test join
+    A="[\"Doiel\", \"Robert\"]"
+    EXPECTED="Doiel, Robert"
+    RESULT=$(bin/string join ", " "${A}")
+    assert_same "join on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "${A}" | bin/string -i - join ", ")
+    assert_same "join on pipe" "$EXPECTED" "$RESULT"
+
+    # Test toupper
+    S="one two three"
+    EXPECTED="ONE TWO THREE"
+    RESULT=$(bin/string toupper "$S")
+    assert_same "toupper on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "$S" | bin/string -i - toupper)
+    assert_same "toupper on pipe" "$EXPECTED" "$RESULT"
+
+    # Test tolower
+    S="ONE TWO THREE"
+    EXPECTED="one two three"
+    RESULT=$(bin/string tolower "$S")
+    assert_same "toupper on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "$S" | bin/string -i - tolower)
+    assert_same "toupper on pipe" "$EXPECTED" "$RESULT"
+
+    # Test totitle
+    S="one two three"
+    EXPECTED="ONE TWO THREE"
+    RESULT=$(bin/string totitle "$S")
+    assert_same "totitle on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "$S" | bin/string -i - totitle)
+    assert_same "totitle on pipe" "$EXPECTED" "$RESULT"
+
+    # Test englishtitle
+    S="the quick brown fox jumped over the lazy dog"
+    EXPECTED="The Quick Brown Fox Jumped Over the Lazy Dog"
+    RESULT=$(bin/string englishtitle "$S")
+    assert_same "englishtitle on args" "$EXPECTED" "$RESULT"
+    RESULT=$(echo -n "$S" | bin/string -i - englishtitle)
+    assert_same "englishtitle on pipe" "$EXPECTED" "$RESULT"
+
+}
+
+#
+# Run the tests
+#
+make bin/string
+test_string
+echo "Success!"
+
+
