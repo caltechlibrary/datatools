aa027e0abc9f67bf9be26ae8a03ab35060628b37
R. S. Doiel <rsdoiel@gmail.com>
2019-06-12 10:37:46 -0700

    prep for v0.0.25 release

--- cmd/xlsx2json/xlsx2json.go
+++ cmd/xlsx2json/xlsx2json.go
@@ -4,7 +4,7 @@
 //
 // @Author R. S. Doiel
 //
-// Copyright (c) 2018, Caltech
+// Copyright (c) 2019, Caltech
 // All rights not granted herein are expressly reserved by Caltech.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
--- datatools.go
+++ datatools.go
@@ -4,7 +4,7 @@
 //
 // @Author R. S. Doiel, <rsdoiel@caltech.edu>
 //
-// Copyright (c) 2017, Caltech
+// Copyright (c) 2019, Caltech
 // All rights not granted herein are expressly reserved by Caltech.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -39,7 +39,7 @@ const (
 	LicenseText = `
 %s %s
 
-Copyright (c) 2017, Caltech
+Copyright (c) 2019, Caltech
 All rights not granted herein are expressly reserved by Caltech.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
