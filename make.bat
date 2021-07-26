6e28961df6a5216a8fac553bb35e71b9b864b3c6
R. S. Doiel <rsdoiel@gmail.com>
2021-07-26 15:48:08 -0700

    sketching make.bat to use on Windows 10 build

---
+++ make.bat
@@ -0,0 +1,69 @@
+@echo off
+REM This is a Windows 10 Batch file for building dataset command
+REM from the command prompt.
+REM
+REM It requires: go version 1.12.4 or better and the cli for git installed
+REM
+go version
+mkdir bin
+echo "Getting ready to build the datatools in bin"
+go build -o bin\codemeta2cff.exe cmd\codemeta2cff\codemeta2cff.go
+go build -o bin\csv2json.exe cmd\csv2json\*.go
+go build -o bin\csv2mdtable.exe cmd\csv2mdtable\*.go
+go build -o bin\csv2xlsx.exe cmd\csv2xlsx\*.go
+go build -o bin\csvcleaner.exe cmd\csvcleaner\*.go
+go build -o bin\csvcols.exe cmd\csvcols\*.go
+go build -o bin\csvfind.exe cmd\csvfind\*.go
+go build -o bin\csvjoin.exe cmd\csvjoin\*.go
+go build -o bin\csvrows.exe cmd\csvrows\*.go
+go build -o bin\finddir.exe cmd\finddir\*.go
+go build -o bin\findfile.exe cmd\findfile\*.go
+go build -o bin\json2toml.exe cmd\json2toml\*.go
+go build -o bin\json2yaml.exe cmd\json2yaml\*.go
+go build -o bin\jsoncols.exe cmd\jsoncols\*.go
+go build -o bin\jsonjoin.exe cmd\jsonjoin\*.go
+go build -o bin\jsonmunge.exe cmd\jsonmunge\*.go
+go build -o bin\jsonrange.exe cmd\jsonrange\*.go
+go build -o bin\mergepath.exe cmd\mergepath\*.go
+go build -o bin\range.exe cmd\range\*.go
+go build -o bin\reldate.exe cmd\reldate\*.go
+go build -o bin\string.exe cmd\string\*.go
+go build -o bin\timefmt.exe cmd\timefmt\*.go
+go build -o bin\toml2json.exe cmd\toml2json\*.go
+go build -o bin\urlparse.exe cmd\urlparse\*.go
+go build -o bin\xlsx2csv.exe cmd\xlsx2csv\*.go
+go build -o bin\xlsx2json.exe cmd\xlsxjson\*.go
+go build -o bin\yaml2json.exe cmd\yaml2json\*.go
+
+echo "Checking compile should see version number of dataset"
+bin\codemeta2cff.exe -version
+bin\csv2json.exe -version
+bin\csv2mdtable.exe -version
+bin\csv2xlsx.exe -version
+bin\csvcleaner.exe -version
+bin\csvcols.exe -version
+bin\csvfind.exe -version
+bin\csvjoin.exe -version
+bin\csvrows.exe -version
+bin\finddir.exe -version
+bin\findfile.exe -version
+bin\json2toml.exe -version
+bin\json2yaml.exe -version
+bin\jsoncols.exe -version
+bin\jsonjoin.exe -version
+bin\jsonmunge.exe -version
+bin\jsonrange.exe -version
+bin\mergepath.exe -version
+bin\range.exe -version
+bin\reldate.exe -version
+bin\string.exe -version
+bin\timefmt.exe -version
+bin\toml2json.exe -version
+bin\urlparse.exe -version
+bin\xlsx2csv.exe -version
+bin\xlsx2json.exe -version
+bin\yaml2json.exe -version
+echo "If OK, you can now copy the dataset.exe to %USERPROFILE%\go\bin"
+echo ""
+echo "      copy bin\* %USERPROFILE%\go\bin
+echo ""
